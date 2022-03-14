575f4eb11b67986848cf835152330bb3ed9c42b7
Rippz <gunes.ozdogan95@gmail.com>
2022-03-14 22:27:55 +0300

    Implement UI of the game

--- script.js
+++ script.js
@@ -1,64 +1,88 @@
+// Event handlers
 const computerPlay = function () {
-  const selections = ["Rock", "Paper", "Scissors"];
-  const rndNum = Math.floor(Math.random() * 3);
+    const selections = ["Rock", "Paper", "Scissors"];
+    const rndNum = Math.floor(Math.random() * 3);
 
-  return selections[rndNum];
+    return selections[rndNum];
 };
 
 const playTurn = function (playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-  if (playerSelection === computerSelection) {
-    return "Tie!";
-  }
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    }
 
-  if (playerSelection === "rock") {
-    if (computerSelection === "scissors") {
-      return "You Win! Rock beats Scissors";
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors";
+        }
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        }
     }
-    if (computerSelection === "paper") {
-      return "You Lose! Paper beats Rock";
-    }
-  }
 
-  if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
-      return "You Win! Paper beats Rock";
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You Win! Paper beats Rock";
+        }
+        if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats Paper";
+        }
     }
-    if (computerSelection === "scissors") {
-      return "You Lose! Scissors beats Paper";
+    if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You Win! Scissors beats Paper";
+        }
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors";
+        }
     }
-  }
-  if (playerSelection === "scissors") {
-    if (computerSelection === "paper") {
-      return "You Win! Scissors beats Paper";
-    }
-    if (computerSelection === "rock") {
-      return "You Lose! Rock beats Scissors";
-    }
-  }
-  return "Please enter valid inputs!";
+    return "Please enter valid inputs!";
 };
 
-const game = function () {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    const result = playTurn(prompt("Select"), computerPlay());
+const handleTurn = function () {
+    result = playTurn(this.textContent, computerPlay());
+    resultDiv = document.querySelector(".results-container");
+    resultDiv.textContent = result;
 
     if (result.includes("Win")) {
-      playerScore++;
+        playerScore++;
     } else if (result.includes("Lose")) {
-      computerScore++;
+        computerScore++;
     }
-    console.log(result);
-    console.log(playerScore, computerScore);
-  }
-  return playerScore > computerScore
-    ? "Player Wins!"
-    : computerScore > playerScore
-    ? "Computer Wins!"
-    : "Tie!";
+
+    playerScoreDiv.textContent = playerScore;
+    computerScoreDiv.textContent = computerScore;
+
+    if (playerScore === 5) {
+        resultDiv.textContent = "You Won the Game!";
+        buttons.forEach((btn) => btn.classList.add("hidden"));
+    } else if (computerScore === 5) {
+        resultDiv.textContent = "You lost the Game!";
+        buttons.forEach((btn) => btn.classList.add("hidden"));
+    }
 };
+
+const restartGame = function () {
+    buttons.forEach((btn) => btn.classList.remove("hidden"));
+    resultDiv.textContent = "Make a Choice";
+    computerScore = 0;
+    playerScore = 0;
+    playerScoreDiv.textContent = playerScore;
+    computerScoreDiv.textContent = computerScore;
+};
+
+// Declarations
+const buttons = document.querySelectorAll(".btn");
+const restartBtn = document.querySelector(".restart");
+const playerScoreDiv = document.querySelector(".player-score");
+const computerScoreDiv = document.querySelector(".computer-score");
+
+// initialize
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((btn) => btn.addEventListener("click", handleTurn));
+restartBtn.addEventListener("click", restartGame);
